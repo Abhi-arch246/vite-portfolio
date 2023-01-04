4fbd74102be8ac31bf0eeac094d847989ff90947
Abhi-arch246 <abhishekreddy235@gmail.com>
2023-01-04 19:26:22 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div>
-      <h1 className="bg-red-400 text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="bg-red-400 text-3xl font-bold underline">Hello world!</h1>
+      <h2></h2>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;
